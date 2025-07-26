ea57917cd796c337a6baf11665faae8846ec739a
unknown <samuelnjoya08@gmail.com>
2025-07-26 12:32:17 +0100

    verification de la pwa

--- sw.js
+++ sw.js
@@ -3,7 +3,18 @@ const urlsToCache = [
     '/',
     '/index.html',
     '/icon-192x192.png',
-    '/icon-512x512.png'
+    '/icon-512x512.png',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap',
+    // Ajoutez les URLs des images utilisées dans le carrousel et les cartes
+    'img/B.jpg',
+    'img/B1.jpg',
+    'img/B2.jpg',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg'
 ];
 
 self.addEventListener('install', event => {
