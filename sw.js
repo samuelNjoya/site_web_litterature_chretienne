4921c900e5bbcdc543f41578d7e75d529d6ce82c
unknown <samuelnjoya08@gmail.com>
2025-07-26 12:56:37 +0100

    verification de la pwa

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ const CACHE_NAME = 'litterature-chretienne-v1';
 const urlsToCache = [
     '/',
     '/index.html',
+    '/offline.html',
     '/icon-192x192.png',
     '/icon-512x512.png',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
